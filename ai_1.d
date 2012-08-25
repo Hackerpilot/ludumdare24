7f620c69bd85f95b05705c5bbc16707733bcc39c
Tom Kelley <distortedsignal@gmail.com>
2012-08-24 21:11:45 -0700

    AI file with function
    AI file now with 100% more function! This still does nothing.

---
+++ ai_1.d
@@ -0,0 +1,13 @@
+/*
+AI file for Ludum Dare 24 started at 8:15 on 8/24/2012
+Tom Kelley
+Here goes nothing
+*/
+
+int doAI(in byte[][] map, int[2] location, int move_score) {
+	/*Assume:
+	For every byte in map, bit order goes (west), (south), (east), (north)
+	Location will always be passed [x,y]
+	move_score will always be positive
+	*/
+}
